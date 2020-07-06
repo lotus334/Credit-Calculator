de025294fed613afc2ad291fb85af578180478b4
Васильев Дмитрий <lotus33497@yandex.ru>
2020-07-06 11:02:07 +0700

    Create Credit Calculator.py
    Add first dialog with user

---
+++ Credit Calculator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 08:27:41 2020
+
+@author: OKS-8
+"""
+
+def mon_payment(principals, period):
+    global payment
+    if principals % period == 0:
+        payment = int(principals / period)
+        return f'Your monthly payment = {payment}'
+    else:
+        payment = int(round(principals / period, 0)) + 1
+        global last_payment
+        last_payment = principals - (period - 1) * payment
+        return f'Your monthly payment = {payment} with last month payment = {last_payment}.'
+
+print('Enter the credit principal:')
+principals = int(input())
+print('What do you want to calculate?')
+print('type "m" - for count of months,')
+print('type "p" - for monthly payment:')
+action = input()
+if action == 'm':
+    print('Enter monthly payment:')
+    mon_pay = int(input())
+    period = int(round(principals / mon_pay, 0))
+    print()
+    print(f'It takes {period} months to repay the credit')
+elif action == 'p':
+    print('Enter count of months:')
+    period = int(input())
+    print()
+    print(mon_payment(principals, period))
